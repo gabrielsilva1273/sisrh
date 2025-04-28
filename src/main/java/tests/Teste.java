0331a2441f0d190d0fcd1b3cd609f405dd9c9bc4
Gabriel Silva <gabrielzw50@gmail.com>
2025-04-28 20:44:28 -0300

    Configuração de testes

---
+++ src/main/java/tests/Teste.java
@@ -0,0 +1,37 @@
+package tests;
+
+
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class Teste {
+	
+	@Test
+    void test1() {
+        
+        assertEquals(5, 5);
+    }
+	@Test
+    void test2() {
+        
+        assertEquals(5, 5);
+    }
+	@Test
+    void test3() {
+        
+        assertEquals(5, 5);
+    }
+	@Test
+    void test4() {
+        
+        assertEquals(5, 5);
+    }
+	@Test
+    void test5() {
+        
+        assertEquals(5, 5);
+    }
+
+}
