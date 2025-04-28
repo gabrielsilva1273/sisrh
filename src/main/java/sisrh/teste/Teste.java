a376b37b859284f3dc84573acb0e31d6903d71f0
Gabriel Silva <gabrielzw50@gmail.com>
2025-04-28 20:19:52 -0300

    Nova configuração dos testes

---
+++ src/main/java/sisrh/teste/Teste.java
@@ -0,0 +1,39 @@
+package sisrh.teste;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class Teste {
+	
+	@Test
+    public void test1() {
+        
+        assertEquals(1,1);
+    }
+	
+	@Test
+    public void test2() {
+        
+        assertEquals(1,1);
+    }
+	
+	@Test
+    public void test3() {
+        
+        assertEquals(1,1);
+    }
+	
+	@Test
+    public void test4() {
+        
+        assertEquals(1,1);
+    }
+	
+	@Test
+    public void test5() {
+        
+        assertEquals(1,1);
+    }
+
+}
